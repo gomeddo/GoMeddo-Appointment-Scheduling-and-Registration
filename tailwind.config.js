65c3b09fb2854abd9129f7961d12a559139b306d
Rizel Thomas <r.thomas@gomeddo.com>
2024-06-14 11:55:20 +0200

    Setup page structure & routing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          medium: "#1376F8",
+          dark: "#011632",
+          black: "#000118",
+        },
+        green: {
+          medium: "#2EC114",
+        },
+      },
+    },
   },
   plugins: [],
 };
