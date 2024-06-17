2a6232741d04fc22ca83467c63080883ad217d4e
Rizel Thomas <r.thomas@gomeddo.com>
2024-06-17 15:11:31 +0200

    Updated header with logo png

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="text-blue-dark px-12 py-8 font-medium sticky top-0 left-0 bg-blue-light">
       <nav className="flex flex-row gap-8 items-center">
         <Link to="/" className="text-2xl font-bold uppercase flex-1">
-          Bright Smiles Dental Clinic
+          <img src="src/assets/logo.png" alt="logo" className="h-8"/>
         </Link>
         <Link>Home</Link>
         <Link>Services</Link>
