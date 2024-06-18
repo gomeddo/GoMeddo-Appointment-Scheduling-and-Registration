2f658d8ba33dc9e54aa54d43dd8ec9358543d523
Rizel Thomas <r.thomas@gomeddo.com>
2024-06-18 13:23:48 +0200

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,24 @@
-import { Link } from "react-router-dom";
+import Logo from "../assets/logo.png";
 
 export default function Header() {
   return (
     <header className="text-blue-dark px-12 py-8 font-medium sticky top-0 left-0 bg-blue-light">
       <nav className="flex flex-row gap-8 items-center">
-        <Link to="/" className="text-2xl font-bold uppercase flex-1">
-          <img src="src/assets/logo.png" alt="logo"/>
-        </Link>
-        <Link>Home</Link>
-        <Link>Services</Link>
-        <Link>About</Link>
-        <Link>Content</Link>
+        <div className="text-2xl font-bold uppercase flex-1">
+          <img src={Logo} alt="Dentist Logo" className="h-7"/>
+        </div>
+        <div className="cursor-default select-none hover:text-blue-dark hover:underline transition-all">
+          Home
+        </div>
+        <div className="cursor-default select-none hover:text-blue-dark hover:underline transition-all">
+          Services
+        </div>
+        <div className="cursor-default select-none hover:text-blue-dark hover:underline transition-all">
+          About
+        </div>
+        <div className="cursor-default select-none hover:text-blue-dark hover:underline transition-all">
+          Contact
+        </div>
       </nav>
     </header>
   );
