27989edad6f80e4cdd95f196e69da7a7a6c4d53e
Rizel Thomas <r.thomas@gomeddo.com>
2024-06-20 14:49:21 +0200

    Update index.jsx

--- src/pages/confirmation/index.jsx
+++ src/pages/confirmation/index.jsx
@@ -48,7 +48,7 @@ export default function ConfirmationPage() {
           <div className="flex justify-end">
             <button
               onClick={handleBackToDashboard}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className="bg-blue-dark hover:bg-blue-hover text-white font-bold py-2 px-4 rounded"
             >
               Back To Dashboard
             </button>
