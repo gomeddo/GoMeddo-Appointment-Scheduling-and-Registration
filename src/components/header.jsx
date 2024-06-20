ebab3ee813f9c2b79b468ee92dbac16269312313
Rizel Thomas <r.thomas@gomeddo.com>
2024-06-20 12:04:45 +0200

    Refactoring header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,31 +1,31 @@
 import Logo from "../assets/logo.png";
-import { Bluetooth } from "react-feather";
+import { Link } from "react-router-dom";
+
+function NavItem(props) {
+  return (
+    <div className="cursor-default select-none hover:text-blue-dark hover:underline transition-all">
+      <Link to={props.to} className="ms-auto hover:scale-105 transition-all">
+        {props.children}
+      </Link>
+    </div>
+  );
+}
 
 export default function Header() {
   return (
-    <header className="text-blue-dark px-12 py-8 font-medium sticky top-0 left-0 bg-blue-light flex flex-col gap-2 lg:gap-0 lg:flex-row">
+    <header className="text-blue-dark px-12 py-8 font-medium sticky top-0 left-0 flex flex-col gap-2 lg:gap-0 lg:flex-row">
       <div className="flex flex-row items-center">
-        {/* Not aligned because font isn't aligned to centerline */}
-        <h1 className="uppercase text-3xl font-bold inline">
+        <h1 className="uppercase text-3xl font-bold">
           Bright Smiles Dental Clinic
         </h1>
-        {/* TODO: Replace with actual tooth icon later */}
-        <Bluetooth className="ml-2 inline" />
+        <img src={Logo} alt="Dentist Logo" className="ml-4 h-6" />
       </div>
 
-      <nav className="lg:ms-auto flex flex-row gap-4 lg:gap-8 items-center font-semibold">
-        <div className="cursor-default select-none hover:text-blue-dark hover:underline transition-all">
-          Home
-        </div>
-        <div className="cursor-default select-none hover:text-blue-dark hover:underline transition-all">
-          Services
-        </div>
-        <div className="cursor-default select-none hover:text-blue-dark hover:underline transition-all">
-          About
-        </div>
-        <div className="cursor-default select-none hover:text-blue-dark hover:underline transition-all">
-          Contact
-        </div>
+      <nav className="lg:ms-auto flex flex-row gap-4 lg:gap-8 items-center font-semibold text-lg">
+        <NavItem to="/">Home</NavItem>
+        <NavItem to="/">Services</NavItem>
+        <NavItem to="/">About</NavItem>
+        <NavItem to="/">Contact</NavItem>
       </nav>
     </header>
   );
