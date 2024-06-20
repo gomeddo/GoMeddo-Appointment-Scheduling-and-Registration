f72d73416817ef380e633365351a99d8a87862a2
Rizel Thomas <r.thomas@gomeddo.com>
2024-06-20 14:49:26 +0200

    Update index.jsx

--- src/pages/appointment/index.jsx
+++ src/pages/appointment/index.jsx
@@ -1,9 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import SampleImage from "../../assets/booking.png";
 
 function FormItem(props) {
   return (
-    <div class="p-4">
-      <label for={props.id} class="block">
+    <div className="p-4">
+      <label htmlFor={props.id} className="block">
         {props.label}
       </label>
       <input
@@ -11,16 +12,26 @@ function FormItem(props) {
         id={props.id}
         name={props.id}
         placeholder={props.label}
-        class="w-full px-3 py-2 mt-1 text-gray-700 border rounded focus:outline-none focus:border-blue-500"
+        className="w-full px-3 py-2 mt-1 text-gray-700 border rounded focus:outline-none focus:border-blue-500"
       />
     </div>
   );
 }
 
 export default function AppointmentPage() {
+  const navigate = useNavigate();
+
+  const handleCancel = () => {
+    navigate("/");
+  };
+
+  const handleSubmit = () => {
+    navigate("/confirmation/:id");
+  };
+
   return (
     <div className="w-full min-h-screen pt-12">
-      <div className="md:max-w-screen-lg	mx-auto">
+      <div className="md:max-w-screen-lg mx-auto">
         <h2 className="text-3xl font-semibold">Make an appointment</h2>
 
         <hr className="my-8 bg-blue-900 h-0.5"></hr>
@@ -37,39 +48,45 @@ export default function AppointmentPage() {
           </div>
 
           <div className="grow ml-24 -mt-4">
-            <div class="grid grid-cols-2 gap-2 gap-y-0">
+            <div className="grid grid-cols-2 gap-2 gap-y-0">
               <FormItem label="First Name" type="text" id="first_name" />
               <FormItem label="Last Name" type="text" id="last_name" />
               <FormItem label="Phone" type="tel" id="phone" />
               <FormItem label="Email" type="email" id="email" />
             </div>
 
-            <div class="">
-              <div class="p-4">
-                <label for="message" class="block">
+            <div>
+              <div className="p-4">
+                <label htmlFor="message" className="block">
                   Message
                 </label>
                 <textarea
                   placeholder="Include a message.."
                   id="message"
-                  class="w-full min-h-24 px-3 py-2 text-gray-700 border rounded focus:outline-none focus:border-blue-500"
+                  className="w-full min-h-24 px-3 py-2 text-gray-700 border rounded focus:outline-none focus:border-blue-500"
                 ></textarea>
               </div>
 
-              <div class="p-4 flex items-center">
+              <div className="p-4 flex items-center">
                 <input
                   type="checkbox"
-                  class="h-5 w-5 mr-2 text-green-500"
+                  className="h-5 w-5 mr-2 text-green-500"
                 ></input>
                 <p>You agree with our friendly privacy policy</p>
               </div>
             </div>
 
-            <div class="px-8 mt-8 flex justify-between">
-              <button class="px-16 py-2 outline rounded-sm hover:bg-gray-100">
+            <div className="px-8 mt-8 flex justify-between">
+              <button
+                className="px-16 py-2 outline rounded-sm hover:bg-gray-100"
+                onClick={handleCancel}
+              >
                 Cancel
               </button>
-              <button class="px-16 py-2 outline rounded-sm hover:bg-gray-100">
+              <button
+                className="px-16 py-2 outline rounded-sm bg-blue-dark hover:bg-blue-hover text-white font-semibold"
+                onClick={handleSubmit}
+              >
                 Submit
               </button>
             </div>
