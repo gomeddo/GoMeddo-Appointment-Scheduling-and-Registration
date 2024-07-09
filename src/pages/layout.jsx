f300e4fbc8786b04c0bfd6f18fab8f9e31322d32
Rizel Thomas <r.thomas@gomeddo.com>
2024-07-09 11:12:04 +0200

    Revert "Update layout.jsx"
    This reverts commit 9d1812fce6378329dd7b4391b7353a2e6ef3e362.

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,19 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/header";
 import FilterProvider from "../sdk/filterContext";
-import StateProvider from "../sdk/stateContext";
 
 export default function Layout() {
   return (
-    <StateProvider>
-      <FilterProvider>
-        <div className="flex flex-col h-screen w-screen overflow-hidden">
-          <Header />
-          <div className="mx-8 px-8 py-6 xl:w-[1280px] xl:mx-auto h-full overflow-auto">
-            <Outlet />
-          </div>
+    <FilterProvider>
+      <div className="flex flex-col h-screen w-screen overflow-hidden">
+        <Header />
+        <div className="mx-8 px-8 py-6 xl:w-[1280px] xl:mx-auto h-full overflow-auto">
+          <Outlet />
         </div>
-      </FilterProvider>
-    </StateProvider>
+      </div>
+    </FilterProvider>
   );
 }
