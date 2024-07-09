123d8381f851656b008c8572c1fc83662eb1e43e
Rizel Thomas <r.thomas@gomeddo.com>
2024-07-09 10:52:48 +0200

    Update index.jsx

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,25 +1,51 @@
 import DentistCard from "./dentistCard";
-import { useDentists } from "../../sdk/hooks";
+import {
+  useDentistResources,
+  useRoomReservationResources,
+  useStaffResources,
+} from "../../sdk/hooks";
 import Filters from "./filters";
 import Skeleton from "../../components/skeleton";
+import { useMemo } from "react";
 
 export default function DashboardPage() {
-  const { isLoading, dentists } = useDentists();
+  const { isLoading: dentistLoading, dentists, rooms } = useDentistResources();
+  const roomIds = useMemo(() => rooms.map((room) => room.id), [rooms]);
+
+  const { staff } = useStaffResources();
+  const { isLoading: reservationLoading, reservations } =
+    useRoomReservationResources(roomIds);
 
   return (
     <div className="flex flex-col gap-6">
       <Filters />
-      {isLoading &&
+      {dentistLoading &&
         Array.from(Array(10).keys()).map((i) => (
           <Skeleton key={i} className="h-96" />
         ))}
-      {!!dentists && (
-        <>
-          {dentists.map((dentists) => (
-            <DentistCard key={dentists.id} {...dentists} />
-          ))}
-        </>
-      )}
+
+      {!dentistLoading &&
+        !!dentists &&
+        !!rooms &&
+        dentists.map((dentist) => {
+          const dentistRooms = rooms.filter(
+            (room) => room.parentId === dentist.id
+          );
+          const roomIds = dentistRooms.map((room) => room.id);
+          const roomReservations = reservations.filter((reservation) =>
+            roomIds.includes(reservation.getCustomProperty("B25__Resource__c"))
+          );
+
+          return (
+            <DentistCard
+              key={dentist.id}
+              dentistResource={dentist}
+              roomResources={dentistRooms}
+              reservationResources={roomReservations}
+              reservationsLoading={reservationLoading}
+            />
+          );
+        })}
     </div>
   );
 }
