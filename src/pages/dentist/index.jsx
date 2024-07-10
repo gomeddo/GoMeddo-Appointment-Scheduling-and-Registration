8a0c112bec0fc7213aeb1e5f0b752316b762b30e
Rizel Thomas <r.thomas@gomeddo.com>
2024-07-10 11:30:30 +0200

    Adjusted gap under title

--- src/pages/dentist/index.jsx
+++ src/pages/dentist/index.jsx
@@ -110,8 +110,8 @@ export default function DentistPage() {
       ) : (
         <div>
           <h2 className="text-3xl font-semibold">Make an appointment</h2>
-          <hr className="bg-gray-300 h-0.5" />
-          <div className="flex flex-row gap-16">
+          <hr className="bg-gray-300 h-0.5 my-4" />
+          <div className="flex flex-row gap-16 items-start">
             <div className="relative">
               <div className="bg-black rounded-3xl w-[14rem] h-[22rem] me-8 mb-8" />
               <img
