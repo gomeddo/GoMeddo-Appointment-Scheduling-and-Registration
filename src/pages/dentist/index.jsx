0ab48fd175b2aa937e3056063bb5e1875734058c
Rizel Thomas <r.thomas@gomeddo.com>
2024-07-10 10:37:14 +0200

    Added loading spinner and navigation to confirmation page

--- src/pages/dentist/index.jsx
+++ src/pages/dentist/index.jsx
@@ -1,5 +1,6 @@
 import { forwardRef, useRef, useState } from "react";
 import { Link, Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import SampleImage from "../../assets/booking.png";
 import Button from "../../components/button";
 import { useStateContext } from "../../sdk/stateContext";
@@ -30,8 +31,10 @@ const FormControl = forwardRef(
 
 export default function DentistPage() {
   const { selectedReservation } = useStateContext();
+  const navigate = useNavigate();
 
   const [formValid, setFormValid] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const firstNameRef = useRef();
   const lastNameRef = useRef();
@@ -58,6 +61,7 @@ export default function DentistPage() {
   const gomeddo = useGomeddo();
   const handleSubmit = async () => {
     try {
+      setIsLoading(true);
       const contact = new Contact(
         firstNameRef.current.value,
         lastNameRef.current.value,
@@ -82,9 +86,11 @@ export default function DentistPage() {
       selectedReservation.setContact(contact);
 
       const response = await gomeddo.saveReservation(selectedReservation);
-      console.log(response);
+      navigate("/confirmation/" + response.id);
+      setIsLoading(false);
     } catch (error) {
       console.error(error);
+      setIsLoading(false);
     }
   };
 
@@ -94,84 +100,95 @@ export default function DentistPage() {
 
   return (
     <div className="flex flex-col gap-8">
-      <h2 className="text-3xl font-semibold">Make an appointment</h2>
-      <hr className="bg-gray-300 h-0.5" />
-      <div className="flex flex-row gap-16">
-        <div className="relative">
-          <div className="bg-black rounded-3xl w-[14rem] h-[22rem] me-8 mb-8" />
-          <img
-            src={SampleImage}
-            alt="Dentist Logo"
-            className="absolute top-8 left-8 rounded-3xl w-[14rem] h-[22rem] object-cover"
-          />
+      {isLoading ? (
+        <div className="flex flex-col items-center justify-center h-full">
+          <div className="w-24 h-24 border-8 border-black border-dashed rounded-full animate-spin"></div>
+          <p className="mt-4 text-xl font-semibold text-black">
+            Your appointment is being made...
+          </p>
         </div>
-        <div className="flex-1 flex flex-col lg:grid lg:grid-cols-2 gap-8">
-          <FormControl
-            ref={firstNameRef}
-            onChange={handleFormChange}
-            label="First Name"
-            type="text"
-            id="first_name"
-            required
-          />
-          <FormControl
-            ref={lastNameRef}
-            onChange={handleFormChange}
-            label="Last Name"
-            type="text"
-            id="last_name"
-            required
-          />
-          <FormControl
-            ref={phoneRef}
-            onChange={handleFormChange}
-            label="Phone"
-            type="tel"
-            id="phone"
-            required
-          />
-          <FormControl
-            ref={emailRef}
-            onChange={handleFormChange}
-            label="Email"
-            type="email"
-            id="email"
-            required
-          />
-          <div className="col-span-2">
-            <label htmlFor="message" className="block">
-              Message
-            </label>
-            <textarea
-              ref={messageRef}
-              placeholder="Include a message.."
-              id="message"
-              className="w-full min-h-24 px-3 py-2 text-gray-700 border rounded focus:outline-none focus:border-blue-500"
-            />
+      ) : (
+        <div>
+          <h2 className="text-3xl font-semibold">Make an appointment</h2>
+          <hr className="bg-gray-300 h-0.5" />
+          <div className="flex flex-row gap-16">
+            <div className="relative">
+              <div className="bg-black rounded-3xl w-[14rem] h-[22rem] me-8 mb-8" />
+              <img
+                src={SampleImage}
+                alt="Dentist Logo"
+                className="absolute top-8 left-8 rounded-3xl w-[14rem] h-[22rem] object-cover"
+              />
+            </div>
+            <div className="flex-1 flex flex-col lg:grid lg:grid-cols-2 gap-8">
+              <FormControl
+                ref={firstNameRef}
+                onChange={handleFormChange}
+                label="First Name"
+                type="text"
+                id="first_name"
+                required
+              />
+              <FormControl
+                ref={lastNameRef}
+                onChange={handleFormChange}
+                label="Last Name"
+                type="text"
+                id="last_name"
+                required
+              />
+              <FormControl
+                ref={phoneRef}
+                onChange={handleFormChange}
+                label="Phone"
+                type="tel"
+                id="phone"
+                required
+              />
+              <FormControl
+                ref={emailRef}
+                onChange={handleFormChange}
+                label="Email"
+                type="email"
+                id="email"
+                required
+              />
+              <div className="col-span-2">
+                <label htmlFor="message" className="block">
+                  Message
+                </label>
+                <textarea
+                  ref={messageRef}
+                  placeholder="Include a message.."
+                  id="message"
+                  className="w-full min-h-24 px-3 py-2 text-gray-700 border rounded focus:outline-none focus:border-blue-500"
+                />
+              </div>
+              <div className="flex items-center col-span-2">
+                <input
+                  ref={agreementRef}
+                  type="checkbox"
+                  id="agreement"
+                  className="h-5 w-5 mr-2 text-green-500"
+                  onChange={handleFormChange}
+                />
+                <p>You agree with our friendly privacy policy</p>
+              </div>
+              <Link to="/">
+                <Button className="w-full">Cancel</Button>
+              </Link>
+              <Button
+                disabled={!formValid}
+                active={formValid}
+                onClick={handleSubmit}
+                className="w-full"
+              >
+                Confirm
+              </Button>
+            </div>
           </div>
-          <div className="flex items-center col-span-2">
-            <input
-              ref={agreementRef}
-              type="checkbox"
-              id="agreement"
-              className="h-5 w-5 mr-2 text-green-500"
-              onChange={handleFormChange}
-            />
-            <p>You agree with our friendly privacy policy</p>
-          </div>
-          <Link to="/">
-            <Button className="w-full">Cancel</Button>
-          </Link>
-          <Button
-            disabled={!formValid}
-            active={formValid}
-            onClick={handleSubmit}
-            className="w-full"
-          >
-            Confirm
-          </Button>
         </div>
-      </div>
+      )}
     </div>
   );
 }
