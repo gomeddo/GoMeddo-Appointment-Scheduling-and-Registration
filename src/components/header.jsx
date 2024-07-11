ca1835a19fbac40b759773142b0ff002760c32e0
Rizel Thomas <r.thomas@gomeddo.com>
2024-07-11 21:28:44 +0200

    Moving hardcoded strings & IDs to constants file

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,13 @@
 import clsx from "clsx";
 import Logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
+import {
+  NAV_HEADER,
+  NAV_HOME,
+  NAV_SERVICES,
+  NAV_CONTACT,
+  NAV_ABOUT,
+} from "../sdk/constants";
 
 function NavItem({ children, disabled, ...rest }) {
   return (
@@ -23,21 +30,19 @@ export default function Header() {
   return (
     <header className="text-blue-dark px-12 py-8 font-medium sticky top-0 left-0 flex flex-col gap-2 lg:gap-0 lg:flex-row bg-white">
       <div className="flex flex-row items-center">
-        <h1 className="uppercase text-3xl font-bold">
-          Bright Smiles Dental Clinic
-        </h1>
+        <h1 className="uppercase text-3xl font-bold">{NAV_HEADER}</h1>
         <img src={Logo} alt="Dentist Logo" className="ml-4 h-6" />
       </div>
       <nav className="lg:ms-auto flex flex-row gap-4 lg:gap-8 items-center font-semibold text-lg">
-        <NavItem to="/">Home</NavItem>
+        <NavItem to="/">{NAV_HOME}</NavItem>
         <NavItem to="/" disabled>
-          Services
+          {NAV_SERVICES}
         </NavItem>
         <NavItem to="/" disabled>
-          About
+          {NAV_ABOUT}
         </NavItem>
         <NavItem to="/" disabled>
-          Contact
+          {NAV_CONTACT}
         </NavItem>
       </nav>
     </header>
--- src/pages/confirmation/index.jsx
+++ src/pages/confirmation/index.jsx
@@ -1,13 +1,30 @@
+import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import SampleImage from "../../assets/booking.png";
 import { CheckCircle } from "react-feather";
+import SampleImage from "../../assets/booking.png";
 import { useReservationResource } from "../../sdk/hooks";
+import {
+  MESSAGE_LOADING,
+  MESSAGE_CONFIRMED,
+  LABEL_CANCELLATION_POLICY,
+  MESSAGE_CANCELLATION_POLICY,
+  LABEL_CLINIC_RATING,
+  LABEL_DENTIST_APPOINTMENT_WITH,
+  LABEL_TOTAL,
+  LABEL_USD,
+  BUTTON_BACK_TO_DASHBOARD,
+  FIELD_RESOURCE_STAFF,
+  FIELD_RESERVATION_RESOURCE_NAME,
+  FIELD_RESERVATION_START_TIME,
+  FIELD_RESERVATION_END_TIME,
+  FIELD_RESERVATION_TOTAL_COST,
+  FIELD_RESERVATION_DURATION,
+  FIELD_RESOURCE_RATING,
+} from "../../sdk/constants";
 
 export default function ConfirmationPage() {
   const { id } = useParams();
-
   const { isLoading, reservation } = useReservationResource(id);
-
   const navigate = useNavigate();
 
   const handleBackToDashboard = () => {
@@ -19,26 +36,32 @@ export default function ConfirmationPage() {
       <div className="flex flex-col items-center justify-center h-full pt-40">
         <div className="w-24 h-24 border-8 border-black border-dashed rounded-full animate-spin"></div>
         <p className="mt-4 text-xl font-semibold text-black">
-          Loading...
+          {MESSAGE_LOADING}
         </p>
       </div>
     );
   }
 
-  const staff = reservation.getCustomProperty("Dentist_Staff__c");
-  const roomName = reservation.getCustomProperty("B25__ResourceName__c");
-  const start = new Date(reservation.getCustomProperty("B25__Start__c"));
-  const end = new Date(reservation.getCustomProperty("B25__End__c"));
-  const cost = reservation.getCustomProperty("B25__Total_Price__c");
-  const duration = reservation.getCustomProperty("Duration_in_Hours__c");
-  const rating = reservation.getCustomProperty("Dentist_Rating__c");
+  const staff = reservation.getCustomProperty(FIELD_RESOURCE_STAFF);
+  const roomName = reservation.getCustomProperty(
+    FIELD_RESERVATION_RESOURCE_NAME
+  );
+  const start = new Date(
+    reservation.getCustomProperty(FIELD_RESERVATION_START_TIME)
+  );
+  const end = new Date(
+    reservation.getCustomProperty(FIELD_RESERVATION_END_TIME)
+  );
+  const cost = reservation.getCustomProperty(FIELD_RESERVATION_TOTAL_COST);
+  const duration = reservation.getCustomProperty(FIELD_RESERVATION_DURATION);
+  const rating = reservation.getCustomProperty(FIELD_RESOURCE_RATING);
 
   return (
     <div className="flex flex-col gap-8">
       <div className="flex gap-2 items-center">
         <div className="items-center rounded-full bg-green-500 py-1.5 px-3 font-sans text-xs font-bold uppercase text-white">
           <CheckCircle className="size-3 inline-block me-1" />
-          Confirmed
+          {MESSAGE_CONFIRMED}
         </div>
         <div className="font-bold">
           Appointment Made For: {start.toLocaleString()}
@@ -56,29 +79,33 @@ export default function ConfirmationPage() {
             />
           </div>
           <div className="w-[16rem]">
-            <div className="font-bold">Cancellation Policy</div>
-            <div>
-              Cancel for free anytime in advance, otherwise you will be charged
-              <span className="font-medium"> 100%</span> of the service price
-              for not showing up.
-            </div>
+            <div className="font-bold">{LABEL_CANCELLATION_POLICY}</div>
+            {MESSAGE_CANCELLATION_POLICY}
           </div>
         </div>
         <div className="flex flex-col gap-8 flex-1">
           <div>
             <div className="font-bold text-2xl">{roomName}</div>
-            <div className="font-medium">5678 West Sunset Boulevard, Los Angeles, CA 90028</div>
-            <div className="font-medium">Clinic Rating: {rating}</div>
+            <div className="font-medium">
+              5678 West Sunset Boulevard, Los Angeles, CA 90028
+            </div>
+            <div className="font-medium">
+              {LABEL_CLINIC_RATING} {rating}
+            </div>
           </div>
           <div className="rounded-lg border border-gray-300 p-5 text-blue-dark max-w-3xl">
             <div className="flex justify-between font-medium text-2xl py-4">
-              <div>Dentist Appointment w/ {staff}</div>
+              <div>
+                {LABEL_DENTIST_APPOINTMENT_WITH} {staff}
+              </div>
               <div className="font-normal">{duration}h</div>
             </div>
             <hr className="border-t border-black opacity-20 my-4 pb-4" />
             <div className="flex justify-between">
-              <div className="font-medium">Total</div>
-              <div>USD: ${cost}</div>
+              <div className="font-medium">{LABEL_TOTAL}</div>
+              <div>
+                {LABEL_USD} {cost}
+              </div>
             </div>
           </div>
           <div className="flex justify-end">
@@ -86,7 +113,7 @@ export default function ConfirmationPage() {
               onClick={handleBackToDashboard}
               className="bg-blue-dark hover:bg-blue-hover text-white font-bold py-2 px-4 rounded"
             >
-              Back To Dashboard
+              {BUTTON_BACK_TO_DASHBOARD}
             </button>
           </div>
         </div>
