0744bdf5df616ce5a49c6115edadf971c4048e68
Rizel Thomas <r.thomas@gomeddo.com>
2024-07-11 21:26:36 +0200

    Update constants.jsx

--- src/sdk/constants.jsx
+++ src/sdk/constants.jsx
@@ -1,18 +1,34 @@
-import React from "react"
+import React from "react";
 
 export const GOMEDDO_KEY = import.meta.env.VITE_GOMEDDO_KEY;
 
 export const FIELD_COUNTRY_ID = "a0Zbn000000gzqHEAQ";
+export const FIELD_RESOURCE_OBJECT = "B25__Resource__c";
+export const FIELD_STAFF_OBJECT = "B25__Staff__c";
+export const FIELD_RESOURCE_STAFF = "Dentist_Staff__c";
+export const FIELD_RESERVATION_RESOURCE_NAME = "B25__ResourceName__c";
+export const FIELD_RESERVATION_START_TIME = "B25__Start__c";
+export const FIELD_RESERVATION_END_TIME = "B25__End__c";
+export const FIELD_RESERVATION_TOTAL_COST = "B25__Total_Price__c";
+export const FIELD_RESERVATION_DURATION = "Duration_in_Hours__c";
+export const FIELD_RESOURCE_RATING = "Dentist_Rating__c";
+export const FIELD_RESOURCE_CITY = "Dentist_City__c";
+export const FIELD_RESOURCE_LOCATION = "Dentist_Location__c";
+export const FIELD_RESOURCE_DEFAULT_PRICE = "B25__Default_Price__c";
+export const FIELD_RESOURCE_IMG = "B25__Image_Url__c";
 
 export const NAV_HEADER = "Bright Smiles Dental Clinic";
 export const NAV_HOME = "Home";
 export const NAV_SERVICES = "Services";
 export const NAV_CONTACT = "Contact";
 export const NAV_ABOUT = "About";
+
 export const MESSAGE_LOADING = "Loading...";
 export const MESSAGE_APPOINTMENT_MADE_FOR = "Appointment Made For: ";
 export const MESSAGE_CONFIRMED = "Confirmed";
-export const LABEL_CANCELLATION_POLICY = "Cancellation Policy";
+export const MESSAGE_NO_CLINIC_MATCH_SEARCH = "No Clinics match your search...";
+export const MESSAGE_LOADING_BOOKING_APPOINTMENT =
+  "Your appointment is being made...";
 export const MESSAGE_CANCELLATION_POLICY = (
   <div>
     Cancel for free anytime in advance, otherwise you will be charged
@@ -20,26 +36,19 @@ export const MESSAGE_CANCELLATION_POLICY = (
     showing up.
   </div>
 );
-export const LABEL_CLINIC_RATING = "Clinic Rating: ";
-export const LABEL_DENTIST_APPOINTMENT_WITH = "Dentist Appointment w/ ";
-export const LABEL_TOTAL = "Total";
-export const LABEL_USD = "USD: ${";
-export const BUTTON_BACK_TO_DASHBOARD = "Back To Dashboard";
 export const MESSAGE_NO_TIME_SLOTS_AVAILABLE =
   "No available time slots currently";
-export const BUTTON_SHOW_MORE = "show less...";
-export const BUTTON_SHOW_LESS = "show more...";
-export const LABEL_SEARCH = "Search...";
-export const LABEL_PRICE = "Price";
-export const LABEL_PRICE_PER_APPOINTMENT = "Price Per Appointment: ";
+
 export const TIME_FRAME_ALL = "All";
 export const TIME_FRAME_MORNING = "Morning";
 export const TIME_FRAME_AFTERNOON = "Afternoon";
 export const TIME_FRAME_EVENING = "Evening";
-export const MESSAGE_NO_CLINIC_MATCH_SEARCH = "No Clinics match your search...";
-export const MESSAGE_LOADING_BOOKING_APPOINTMENT =
-  "Your appointment is being made...";
+
 export const LABEL_MAKE_APPOINTMENT = "Make an appointment";
+export const LABEL_SEARCH = "Search...";
+export const LABEL_PRICE = "Price";
+export const LABEL_CANCELLATION_POLICY = "Cancellation Policy";
+export const LABEL_PRICE_PER_APPOINTMENT = "Price Per Appointment: ";
 export const LABEL_FIRST_NAME = "First Name";
 export const LABEL_LAST_NAME = "Last Name";
 export const LABEL_PHONE = "Phone";
@@ -48,28 +57,13 @@ export const LABEL_MESSAGE = "Message";
 export const LABEL_MESSAGE_PLACEHOLDER = "Include a message..";
 export const LABEL_AGREEMENT_POLICY =
   "You agree with our friendly privacy policy";
+export const LABEL_CLINIC_RATING = "Clinic Rating: ";
+export const LABEL_DENTIST_APPOINTMENT_WITH = "Dentist Appointment w/ ";
+export const LABEL_TOTAL = "Total";
+export const LABEL_USD = "USD: $";
+
 export const BUTTON_CANCEL = "Cancel";
 export const BUTTON_CONFIRM = "Confirm";
-
-export const FIELD_RESOURCE_STAFF = "Dentist_Staff__c";
-export const FIELD_RESERVATION_RESOURCE_NAME = "B25__ResourceName__c";
-export const FIELD_RESERVATION_START_TIME = "B25__Start__c";
-export const FIELD_RESERVATION_END_TIME = "B25__End__c";
-export const FIELD_RESERVATION_TOTAL_COST = "B25__Total_Price__c";
-export const FIELD_RESERVATION_DURATION = "Duration_in_Hours__c";
-export const FIELD_RESOURCE_RATING = "Dentist_Rating__c";
-
-export const FIELD_RESOURCE_CITY = "Dentist_City__c";
-export const FIELD_RESOURCE_LOCATION = "Dentist_Location__c";
-export const FIELD_RESOURCE_PRICE = "B25__Default_Price__c";
-export const FIELD_RESOURCE_IMG = "B25__Image_Url__c";
-// export const = "";
-// export const = "";
-// export const = "";
-// export const = "";
-// export const = "";
-// export const = "";
-// export const = "";
-// export const = "";
-// export const = "";
-// export const = "";
+export const BUTTON_SHOW_MORE = "show less...";
+export const BUTTON_SHOW_LESS = "show more...";
+export const BUTTON_BACK_TO_DASHBOARD = "Back To Dashboard";
